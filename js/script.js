5b61e481b688a440d2537ba23ef93416c6f4fa80
jWaitoa <jackwaitoa@gmail.com>
2024-05-14 13:30:46 +1200

    refactor: refactored JS code for better readability
    Checks for existing sizes in a new function (cartHasExistingSize)
    Adding new item row is in another function (createNewRow)

--- js/script.js
+++ js/script.js
@@ -41,15 +41,13 @@ window.onload = function () {
 };
 
 let shoppingCart = document.querySelector(".shopping-cart");
-let card = document.querySelector(".card");
-let quantity = document.querySelector(".quantity");
-let list = document.querySelector(".list");
-let listCard = document.querySelector(".listCard");
-
 let isCartOpen = false;
 
 // Opens / Closes shopping cart
 shoppingCart.addEventListener("click", () => {
+
+  let card = document.querySelector(".card");
+
   if (!isCartOpen) {
     card.classList.add("active");
     shoppingCart.classList.add("active");
@@ -61,59 +59,69 @@ shoppingCart.addEventListener("click", () => {
   }
 });
 
-// Add event listener to the Add to Cart button
-let currentQuantity = 0;
+let totalQuantity = 0;
+// Adds and updates items in cart
 function addToCart() {
-  // Get the selected size
   let selectedSize = document.querySelector(".sizeButton.selected").textContent;
+
+  // Increment cart quantity
+  totalQuantity++;
+  document.querySelector(".quantity").textContent = `(${totalQuantity})`;
+
+  let existingSize = cartHasExistingSize(selectedSize);
+  if (!existingSize) {
+    // Creates a new item in cart
+    createNewRow(selectedSize);
+  }
+}
+
+// Looks for existing sizes in cart and updates quantity if true.
+function cartHasExistingSize(selectedSize) {
+  let sizeExists = false;
+  // Checks if any items are in cart.
+  var cards = document.getElementById("card").childElementCount;
+  if (cards > 0) {
+    var cards = document.querySelectorAll(".card > div");
+    cards.forEach((cartItem) => {
+      // Get the size of the current cart item
+      let itemSize = cartItem.querySelector(".cart-size").textContent;
+
+      // Check if the current item's size matches the selected size
+      if (itemSize === selectedSize) {
+        // Increment the quantity of the current item
+        let quantityElement = cartItem.querySelector(".quantity");
+        let newQuantity = parseInt(quantityElement.textContent) + 1;
+        quantityElement.textContent = newQuantity;
+        sizeExists = true;
+      }
+    });
+  }
+  return sizeExists;
+}
+
+let currentHeight = 0;
+function createNewRow(selectedSize) {
   let title = document.getElementById("title").textContent;
   let price = document.getElementById("price").textContent;
-  let quantity = 1;
 
   var cartRow = document.createElement("div");
   cartRow.innerText = selectedSize;
   var cartItems = document.getElementsByClassName("card")[0];
 
-  // checks if the item is already in the cart
-  let itemExists = false;
-
-  // Checks if any items are in cart.
-  var cards = document.getElementById("card").childElementCount;
-  if (cards > 0) {
-
-    var cards = document.querySelectorAll(".card > div");
-    cards.forEach((cartItem) => {
-      // Get the size of the current cart item
-      let itemSize = cartItem.querySelector(".cart-size").textContent;
-      console.log("ItemSize" + itemSize + " SelectedSize " + selectedSize);
-
-      // Check if the current item's size matches the selected size
-      if (itemSize === selectedSize) {
-        // Increment the quantity of the existing item
-        let quantityElement = cartItem.querySelector(".quantity");
-        let newQuantity = parseInt(quantityElement.textContent) + 1;
-        quantityElement.textContent = newQuantity;
-        itemExists = true;
-      }
-    });
-  }
-  if (!itemExists) {
-
-    // Creates a new cart row
-    var cartRowContents = `
+  // Creates a new cart row
+  var cartRowContents = `
     <div class="cart-row">
         <img class="cart-img" src="images/classic-tee.jpg">
         <div class="cart-column">
             <p>${title}</p>
-            <div>
-            <span class="quantity">
-            ${quantity}
-            </span>
-            x ${price}
+            <div><span class="quantity" id="quantity">1</span> x ${price}</div>
             <p>Size: <span class="cart-size" id="cart-size">${selectedSize}</span></p>
         </div>
     `;
-    cartRow.innerHTML = cartRowContents;
-    cartItems.append(cartRow);
-  }
+  cartRow.innerHTML = cartRowContents;
+  cartItems.append(cartRow);
+
+  // Increments height of the cart
+  currentHeight += 30;
+  document.getElementById("card").style.height = currentHeight + "vh";
 }
